4cf9dc7e0d6cae9f3d8f7de002eca3e22d6752de
chayashegde <48047461+chayashegde@users.noreply.github.com>
2020-01-02 17:14:57 +0530

    Test code pushed to repository.

---
+++ Arithmetic operators/Main.java
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+int a,b;
+  cin>>a;
+  cin>>b;
+  int c=a+b;
+  int d=a-b;
+  int e=a*b;
+  int f=a%b;
+  int g=a/b;
+  cout<<c<<"\n";
+    cout<<d<<"\n";
+   cout<<e<<"\n";
+   cout<<f<<"\n";
+  cout<<g;
+  
+}
---
+++ Round Off/Main.java
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+  float a;
+  cin>>a;
+  int b=floor(a);
+  int c=ceil(a);
+  int d=floor(a);
+  cout<<b<<"\n"<<c<<"\n"<<d;
+
+}
